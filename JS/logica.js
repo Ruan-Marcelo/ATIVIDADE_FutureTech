04598d05db414f22a9223b0f9ce2b3efe0b0bf02
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-05-15 18:59:36 -0300

    Function Soma

--- JS/logica.js
+++ JS/logica.js
@@ -1,5 +1,12 @@
+// Função para processar madeira e criar um produto final
 function processarMadeira(material){
     let produtoFinal = "Cadeira de " + material;
     return produtoFinal;
 }
 console.log(processarMadeira("Ferro"));
+
+// Função para somar valor a com valor b
+function soma(a, b){
+    return a + b;
+}
+console.log(soma(5, 3));
