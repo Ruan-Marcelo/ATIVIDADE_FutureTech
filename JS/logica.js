6be14833cb94aa0603ae6698b7ca0e6a4f4b2143
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-05-15 19:25:33 -0300

    Saudação e function calcular media

--- JS/logica.js
+++ JS/logica.js
@@ -24,3 +24,17 @@ function realParaDolar(valorReal, cotacaoDolar){
     return total;
 }
 console.log(realParaDolar(7.89, 5.08));
+
+// chamar function
+
+function saudacao(){
+    console.log("Olá, seja bem-vindo!");
+}
+saudacao();
+
+function calcularMedia(n1, n2){
+    var media = (n1 + n2) / 2;
+}
+
+var minhaMedia = calcularMedia(8, 6);
+console.log("A média é: " + minhaMedia);
