94fda5d99acdb18facd296c125e5d5c35775dd29
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-05-15 19:06:04 -0300

    function valor do dolar com cotação real

--- JS/logica.js
+++ JS/logica.js
@@ -18,3 +18,9 @@ function somar(v1, v2){
 console.log("Pedido 1:", somar(10, 20));
 console.log("Pedido 2:", somar(15, 25));
 console.log("Pedido 3:", somar(20, 30));
+
+function realParaDolar(valorReal){
+    var total = valorReal * cotacaoDolar;
+    return total;
+}
+console.log(realParaDolar(7.89, 5.08));
