7a855c6f302f6cba19bd4ec4069d7a0a405e796b
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-05-22 19:30:59 -0300

    Aula de JS Class

---
+++ JS/POOJS/class.js
@@ -0,0 +1,10 @@
+class Carro{
+    contructor(marca, modelo, ano){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    buzinar(){
+        console.log("Buzinando...");
+    }
+}
